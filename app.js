6a15d6bb83489e68529f3fd2fbc13ea77593070b
Ajay Singla <ajay.singla@appwrk.com>
2021-01-21 21:01:22 +0530

    Working on dynamic collection

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const schema = require('./schema')
 var cors = require('cors')
 
 const app = express();
- app.use(cors())
+app.use(cors())
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://ajay_singla:H1PJ64EK1hEB4Eya@cluster0.iimcq.mongodb.net/spincv?retryWrites=true&w=majority')
@@ -13,11 +13,41 @@ mongoose.connection.once('open', () => {
     console.log('conneted to database');
 });
 
-app.use("/",function(req, res){
+app.use("/create", function (req, res) {
     console.log("Create update schema");
+
+     var Schema = mongoose.Schema;
+    var thingSchema = new Schema({}, { strict: false, collection: 'mycollection' });
+
+    //strict, if true then values passed to our model constructor that were not specified in our schema do not get saved to the db.
+   //collection, for prevent from auto append 's'.
+   
+   var Thing = mongoose.model('mycollection', thingSchema);
+   var obj={
+       name:"ajay singla"
+   }
+   var thing = new Thing(obj);
+   thing.save();
+
+  
+
+    // var SomeModelSchema = new Schema({
+    //     a_string: String,
+    // });
+
+    // // Compile model from schema
+    //  var SomeModel =    mongoose.model('SomeModel', SomeModelSchema, "SomeModel");
+
+    // let modelToSave = new SomeModel({
+    //     a_string: "ajay singla",
+    // })
+    // modelToSave.save()
+
     res.send("API is running")
-    
+
 })
+
+
 
 //This route will be used as an endpoint to interact with Graphql, 
 //All queries will go through this route. 
@@ -28,7 +58,6 @@ app.use('/graphql', graphqlHTTP({
     //which provides an interface to make GraphQl queries
     graphiql: true
 }));
-
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 }); 
