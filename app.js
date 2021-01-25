ce936a9023f4a2dac3dd20293a7de0cb494206cd
Ajay Singla <ajay.singla@appwrk.com>
2021-01-25 12:12:37 +0530

    Disable field API added

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const schema = require('./schema')
 var cors = require('cors')
+var ObjectId = require('mongodb').ObjectID;
 
 const app = express();
+// middleware
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(cors())
 // const mongoose = require('mongoose');
 
@@ -12,14 +16,14 @@ app.use(cors())
 // mongoose.connection.once('open', () => {
 //     console.log('conneted to database');
 // });
-
+var dbo;
 var MongoClient = require('mongodb').MongoClient;
 const url =
   "mongodb+srv://ajay_singla:H1PJ64EK1hEB4Eya@cluster0.iimcq.mongodb.net/spincv?retryWrites=true&w=majority";
 
 MongoClient.connect(url, function (err, db) {
   if (err) throw err;
-  var dbo = db.db("spincv");
+  dbo = db.db("spincv");
   //Create a collection name "customers":
   // dbo.createCollection("HomePage", function (err, res) {
   //   if (err) throw err;
@@ -27,61 +31,80 @@ MongoClient.connect(url, function (err, db) {
   //   db.close();
   // });
 
-  var myobj = { name: "APPWRK IT SOLUTIONS PVT LTD.", address: "Chandigarh, India", contact:"9416269166" };
-  dbo.collection("customers").insertOne(myobj, function(err, res) {
-    if (err) throw err;
-    console.log("1 document inserted");
-    db.close();
-  });
+  // var myobj = { name: "APPWRK IT SOLUTIONS PVT LTD.", address: "Chandigarh, India", contact:"9416269166" };
+  // dbo.collection("customers").insertOne(myobj, function(err, res) {
+  //   if (err) throw err;
+  //   console.log("1 document inserted");
+  //   db.close();
+  // });
 });
 
-app.use("/create", function (req, res) {
-    console.log("Create update schema");
+app.use("/getCollection", async function (req, res) {
 
-     var Schema = mongoose.Schema;
-    var thingSchema = new Schema({}, { strict: false, collection: 'mycollection' });
+  let data = await dbo.collection("Pages").find({}).toArray();
+  res.json(data);
 
-    //strict, if true then values passed to our model constructor that were not specified in our schema do not get saved to the db.
-   //collection, for prevent from auto append 's'.
-   
-   var Thing = mongoose.model('mycollection', thingSchema);
-   var obj={
-       name:"ajay singla"
-   }
-   var thing = new Thing(obj);
-   thing.save();
+});
 
-  
+app.use("/saveField", async function (req, res) {
+  let data = req.body;
 
-    // var SomeModelSchema = new Schema({
-    //     a_string: String,
-    // });
+  let dataToSave = {};
+  data.forEach(element => {
+    dataToSave[element.fieldName] = element.fieldValue;
+  });
+  dbo.collection("HomePage").insertOne(dataToSave, function (err) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    res.send("Done");
+  });
 
-    // // Compile model from schema
-    //  var SomeModel =    mongoose.model('SomeModel', SomeModelSchema, "SomeModel");
 
-    // let modelToSave = new SomeModel({
-    //     a_string: "ajay singla",
-    // })
-    // modelToSave.save()
+});
 
-    res.send("API is running")
+app.use("/disableField", async function (req, res) {
+  let data = req.body;
+  await dbo.collection("Pages").updateOne(
+    { _id: ObjectId(data.id) },
+    { $set: { "disabled": data.disable } });
+  res.send("Done")
+
+});
+
+app.use("/createCollection", function (req, res) {
+
+  // dbo.createCollection("Pages", function (err, res) {
+  //   if (err) throw err;
+  //   console.log("Collection created!");
+  //   db.close();
+  // });
+
+  let myObj = {
+    fieldName: req.body.fieldName,
+    fieldType: req.body.fieldType,
+    page: req.body.page
+  }
+  dbo.collection("Pages").insertOne(myObj, function (err) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    res.send("Done");
+  });
 
 })
 
 
 
-// //This route will be used as an endpoint to interact with Graphql, 
-// //All queries will go through this route. 
-// app.use('/graphql', graphqlHTTP({
-//     //directing express-graphql to use this schema to map out the graph 
-//     schema,
-//     //directing express-graphql to use graphiql when goto '/graphql' address in the browser
-//     //which provides an interface to make GraphQl queries
-//     graphiql: true
-// }));
+//This route will be used as an endpoint to interact with Graphql, 
+//All queries will go through this route. 
+app.use('/graphql', graphqlHTTP({
+  //directing express-graphql to use this schema to map out the graph 
+  schema,
+  //directing express-graphql to use graphiql when goto '/graphql' address in the browser
+  //which provides an interface to make GraphQl queries
+  graphiql: true
+}));
 
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000');
+  console.log('Listening on port 3000');
 }); 
